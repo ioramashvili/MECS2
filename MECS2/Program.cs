74a3debc72ce3170f5f2194105e94a8e65ab1238
shota ioramashvili <shota ioramashvili>
2017-09-22 22:18:50 +0400

    Add project files.

---
+++ MECS2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MECS2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int integer = 0;
+            string text = "some text";
+            char c = 'a';
+            double d = 1.1;
+
+            if (integer > 0)
+            {
+                Console.WriteLine(integer);
+            } else if (text.Length > 0)
+            {
+                Console.WriteLine(text);
+            }
+        }
+    }
+}
